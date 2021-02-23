45dead9c1551f4f3c5d9a2c9ad8dfc1da2346da5
rihardslozins <rihards.lozins@gmail.com>
2021-02-23 13:30:36 +0200

    Logo added in Navbar (Chat View)

--- DSirius/Controllers/ChatViewController.swift
+++ DSirius/Controllers/ChatViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
     
@@ -14,10 +15,41 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Hides back button
+        navigationItem.hidesBackButton = true
+        addNavBarImage()
+        
+    }
+    
+    func addNavBarImage() {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "Dsirius_Logo"))
+        imageView.frame = CGRect(x: 0, y: 0, width: 170, height: 30)
+        imageView.contentMode = .scaleAspectFit
+        
+        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: 170, height: 30))
+        
+        titleView.addSubview(imageView)
+        titleView.backgroundColor = .clear
+        self.navigationItem.titleView = titleView
     }
     
     @IBAction func sendPressed(_ sender: Any) {
         
     }
     
-}
+    @IBAction func logoOutPressed(_ sender: UIBarButtonItem) {
+        
+        
+        do {
+            try Auth.auth().signOut()
+            
+            //Navigates to start screen when Log Out is pressed
+            navigationController?.popToRootViewController(animated: true)
+            
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+    
+}//End
